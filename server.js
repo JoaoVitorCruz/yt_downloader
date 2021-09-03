6cf174be782c2274ad7904c68e2aaf194a72140b
João Vitor Mauricio Cruz <82549355+JoaoVitorCruz@users.noreply.github.com>
2021-09-03 06:17:32 -0300

    finalizado

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const fs = require('fs')
+const ytdl = require('ytdl-core')
+const app = express()
+const path = require('path')
+const { response } = require('express')
+
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+app.use(express.static(path.resolve(__dirname, 'public')))
+
+app.get('/', (request,response, next) => {
+    response.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+})
+
+app.get('/videoInfo', async (req,res,next) => {
+    const videoUrl = req.query.videoURL
+    const info = await ytdl.getInfo(videoUrl)
+    res.status(200).json(info)
+    
+    response.status(200)
+
+})
+
+app.get('/download',(req,res,next) => {
+    let videoURL = req.query.videoURL
+    let itag = req.query.itag
+    
+    res.header('Content-disposition','attachment;\ filename="video.mp4"')
+    
+    ytdl(videoURL, {
+        filter: format => format.itag == itag
+    }).pipe(res)
+})
+
+app.listen(3333, ()=>{
+    console.log('http://localhost:3333')
+})
